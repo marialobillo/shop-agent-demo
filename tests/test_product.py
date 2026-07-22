c61a9191eb99c5c4501c2de66edb691bbf7c8ce2
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2026-07-22 17:02:02 +0200

    test: add product price can be zero

--- tests/test_product.py
+++ tests/test_product.py
@@ -7,3 +7,8 @@ from tienda.domain.product import Product
 def test_product_price_should_be_positive_or_zero():
     with pytest.raises(ValueError):
         Product("product_id", "pair of jeans", -3000)
+
+def test_product_price_can_be_zero():
+    new_product = Product("product_id", "pair of jeans", 0)
+
+    assert new_product.price == 0
